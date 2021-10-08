c3822324358cd9f06d411aa48b1364a204a6e2fe
Ellie Judge <judge_ellie.com>
2021-10-08 14:40:55 +0100

    adds more kitties

--- script.js
+++ script.js
@@ -2,15 +2,23 @@ const pets= [
     {
         "name" : "Purrsloud",
         "species" : "Cat",
-        "favFoods" : ["wet food", "dry food", "<strong>any</strong> food"],
+        "favFoods" : ["wet food", "dry food", "wet food"],
         "birthYear" : 2016,
         "photo" : "https://learnwebcode.github.io/json-example/images/cat-2.jpg"
     },
     {
-        "name" : "Barksalot",
-        "species" : "Dog",
+        "name" : "Mr Gwumpy",
+        "species" : "Cat",
+        "favFoods" : ["caviar"],
+        "birthYear" : 2016,
+        "photo" : "https://cdn.cnn.com/cnnnext/dam/assets/190517103414-01-grumpy-cat-file-restricted.jpg"
+    },
+    {
+        "name" : "Jumpy",
+        "species" : "Scaredy Cat",
+        "favFoods" : ["not cucumbers!"],
         "birthYear" : 2008,
-        "photo" : "https://learnwebcode.github.io/json-example/images/dog-1.jpg"
+        "photo" : "https://i.ytimg.com/vi/eX2qFMC8cFo/maxresdefault.jpg"
     },
     {
         "name" : "Meowsalot",
@@ -20,19 +28,48 @@ const pets= [
         "photo" : "https://learnwebcode.github.io/json-example/images/cat-1.jpg"
     },
     {
-        "name" : "Test",
+        "name" : "Cindy Clawford",
         "species" : "Cat",
         "favFoods" : ["mice"],
         "birthYear" : 2012,
         "photo" : "https://icatcare.org/app/uploads/2018/07/Thinking-of-getting-a-cat.png"
+    },
+    {
+        "name" : "Katy Purry",
+        "species" : "Cat",
+        "favFoods" : ["bugs"],
+        "birthYear" : 2015,
+        "photo" : "https://www.rd.com/wp-content/uploads/2021/03/GettyImages-1183822926.jpg"
+    },
+    {
+        "name" : "Dopey",
+        "species" : "Cat",
+        "favFoods" : ["bugs"],
+        "birthYear" : 2018,
+        "photo" : "https://i1.wp.com/katzenworld.co.uk/wp-content/uploads/2019/06/funny-cat.jpeg?fit=1920%2C1920&ssl=1"
+    },
+    {
+        "name" : "Blobby",
+        "species" : "Cat",
+        "favFoods" : ["your soul"],
+        "birthYear" : 2020,
+        "photo" : "https://www.top13.net/wp-content/uploads/2015/10/perfectly-timed-cat-photos-funny-cover.jpg"
     }
+
 ];
 
 (function () {
-    const petContainers = document.getElementsByClassName("pet-pic");
-    for (let i = 0; i < petContainers.length; i++) {
-        petContainers[i].src = pets[i].photo;
+    const petImage = document.getElementsByClassName("pet-pic") // img
+    const petNames = document.getElementsByClassName("pet-name") // h3
+
+    for (let i = 0; i < petImage.length; i++) {
+        petImage[i].src = pets[i].photo;
     }
+
+
+    // for (let i = 0; i < petNames.length; i++) {
+    //     petNames[i].innerText = pets[i].name;
+    // }
 })();
 
 
